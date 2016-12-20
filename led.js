22d1cfbf06415e98e685942de3b5df0f3fdfe44a
Carlos Abdalla <abdalla@msdev.com.br>
2016-12-19 21:54:42 -0500

    cool

--- led.js
+++ led.js
@@ -1,11 +1,12 @@
-const Tessel = require("tessel-io");
-const five = require("johnny-five");
+const Tessel = require('tessel-io');
+const five = require('johnny-five');
 
 const board = new five.Board({
     io: new Tessel()
 });
 
-board.on("ready", () => {
-    var led = new five.Led("a5");
-    led.blink(500);
+board.on('ready', () => {
+    const led = new five.Led('a5');
+    //led.blink(500);
+    led.pulse(500);
 });
